47e7c31e443d772101543be90ecf7ea3c3618b42
VJ-Duardo <olafstein501@gmail.com>
2020-05-16 18:00:36 +0200

    init commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var count = 1000;
+var content = document.getElementById("content");
+
+for (let i = 0; i<count; i++){
+    var img = document.createElement("img");
+    img.src = "https://static-cdn.jtvnw.net/emoticons/v1/"+i+"/2.0";
+    content.appendChild(img);
+}
