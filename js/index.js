8c7696ba388c207fd551bb0e4198151bc5f5752e
VJ-Duardo <olafstein501@gmail.com>
2020-05-16 18:36:05 +0200

    added pages for all emotes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
-var count = 50000;
+var count = 1800000;
+var pageAmount = 5000;
 var content = document.getElementById("content");
+var thisSite = "https://vj-duardo.github.io/AllTwitchEmotes/";
+var regex = thisSite + '\\??';
 
-for (let i = 0; i<count; i++){
+var currentPage = parseInt(window.location.href.replace(new RegExp(regex,"g"), ''));
+if (typeof currentPage === 'undefined' || isNaN(currentPage))
+    window.location.href = thisSite + '?' + 0;
+
+for (let i = currentPage*pageAmount; i<(currentPage*pageAmount)+pageAmount; i++){
     var img = document.createElement("img");
+    img.setAttribute("title", i);
     img.src = "https://static-cdn.jtvnw.net/emoticons/v1/"+i+"/2.0";
     content.appendChild(img);
+}
+
+function next(){
+    var page = parseInt(window.location.href.replace(new RegExp(regex,"g"), ''))+1;
+    if (typeof page === 'undefined' || isNaN(page))
+        page = 0;
+    window.location.href = thisSite + '?' + page;
 }
